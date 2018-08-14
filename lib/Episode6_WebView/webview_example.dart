f24b867c71b16c079bc27d2dcd42ab4dbc595296
Pawan Kumar <pawan221b@gmail.com>
2018-08-14 20:42:03 +0530

    Episode 6 added - Webview

---
+++ lib/Episode6_WebView/webview_example.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  TextEditingController controller = TextEditingController();
+  FlutterWebviewPlugin flutterWebviewPlugin = FlutterWebviewPlugin();
+  var urlString = "https://google.com";
+
+  launchUrl() {
+    setState(() {
+      urlString = controller.text;
+      flutterWebviewPlugin.reloadUrl(urlString);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    flutterWebviewPlugin.onStateChanged.listen((WebViewStateChanged wvs) {
+      print(wvs.type);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      appBar: AppBar(
+        title: TextField(
+          autofocus: false,
+          controller: controller,
+          textInputAction: TextInputAction.go,
+          onSubmitted: (url) => launchUrl(),
+          style: TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: "Enter Url Here",
+            hintStyle: TextStyle(color: Colors.white),
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.navigate_next),
+            onPressed: () => launchUrl(),
+          )
+        ],
+      ),
+      url: urlString,
+      withZoom: false,
+    );
+  }
+}
